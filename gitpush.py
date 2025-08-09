c0265d1b978a78030dc999e1375b05e3b5304e9e
Богданов Сергей Викторович <SeViBogdanov@vimpelcom.ru>
2025-08-09 22:00:13 +0300

    upd

---
+++ gitpush.py
@@ -0,0 +1,6 @@
+import os
+import time
+os.system('cd C:\\Users\\sevibogdanov\\Desktop\\tvtolstova.github.io && git add .')
+os.system('cd C:\\Users\\sevibogdanov\\Desktop\\tvtolstova.github.io && git commit -m upd')
+os.system('cd C:\\Users\\sevibogdanov\\Desktop\\tvtolstova.github.io && git push')
+time.sleep(10)
